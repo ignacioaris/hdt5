bcc26a7251cd61e8f8c456d8ac2b552b99ef6634
ignacioaris <arisignacio90@gmail.com>
2025-03-05 07:22:19 -0600

    Update simulador.py

--- simulador.py
+++ simulador.py
@@ -2,3 +2,9 @@ import simpy
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+
+RANDOM_SEED = 42
+MEMORIA_TOTAL = 100
+INSTRUCCIONES_POR_UNIDAD_TIEMPO = 3
+INTERVALO_LLEGADA_PROCESOS = 1
+random.seed(RANDOM_SEED)
